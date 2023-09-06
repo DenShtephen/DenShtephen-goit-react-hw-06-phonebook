07d3a24b06c1183ea313e7da17d6840bc209a5a4
DenShtephen <135002535+DenShtephen@users.noreply.github.com>
2023-09-06 03:04:17 +0300

    add some Redux

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
+import { store } from './store/store';
 import './index.css';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="goit-react-hw-06-phonebook">
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
